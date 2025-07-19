a163cab9060f095ffbd53f8e6a2ebcdab62a34b9
pateldhruuuv <pateldhruv200613@gmail.com>
2025-07-19 21:41:26 +0530

    update

--- Trial/src/main/java/Stock_Predictor/DerivedIndicators.java
+++ Trial/src/main/java/Stock_Predictor/DerivedIndicators.java
@@ -56,6 +56,5 @@ public class DerivedIndicators {
 
         return (sum / vsum);
     }
-
-
+    //vwap
 }
--- Trial/src/main/java/Stock_Predictor/Test.java
+++ Trial/src/main/java/Stock_Predictor/Test.java
@@ -4,10 +4,9 @@ public class Test {
     public static void main(String[] args) {
 
         CSV_Manager csvManager = new CSV_Manager();
-        csvManager.readCSV("C:\\Users\\patel\\Downloads\\Stock_Files\\543940.csv", "Stock_Unknown");
+        csvManager.readCSV("D:\\543940.csv", "Stock_Unknown");
         // csvManager.readCSV("C:\\Users\\patel\\Downloads\\Stock_Files\\NIFTY 50-13-07-2024-to-13-07-2025.csv","Stock_Unknown1");
-
-        csvManager.viewStockTimePeriod("Stock_Unknown");
+        csvManager.viewStock("Stock_Unknown");
         //csvManager.viewStock("Stock_Unknown");
 
 
